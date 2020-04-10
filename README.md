2ef454a2b1b7c0dc94f66ea581e4c0bcfa0ae076
lxiong1 <luexionglx@gmail.com>
2020-04-10 14:09:54 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-![master](https://github.com/lxiong1/cloud-temperature-humidity-system/workflows/master/badge.svg)
+![master](https://github.com/lxiong1/cloud-temperature-humidity-system/workflows/master/badge.svg) [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/) [![made-with-c](https://img.shields.io/badge/Made%20with-C-1f425f.svg)](https://en.cppreference.com/w/) [![made-with-latex](https://img.shields.io/badge/Made%20with-LaTeX-1f425f.svg)](https://www.latex-project.org/)
 
 # Temperature & Humidity Notification System
 
-The purpose of this project is to be able to notify a user via SMS when their ambient room temperature and relative humidity are in the range for discomfort and potential health risks. This is accomplished using a mixture of specified hardware and cloud technologies mentioned below.
+The purpose of this project is to be able to notify a user via SMS when their ambient room temperature and relative humidity are in the range for discomfort and potential health risks. It is an Internet of Things (IoT) project, built with an event-driven serverless architecture and accomplished using a mixture of specified hardware and cloud technologies mentioned below.
 
 ## Hardware
 - Particle Argon
@@ -17,19 +17,30 @@ The following is the breadboard schematic:
 
 ## Technology Stack
 - Python
+- C
+- Github Actions
+- Docker
+- NGINX
+- Plotly
 - Particle Cloud
 - Google Cloud Platform
-    - Functions
-    - PubSub
+    - Compute Engine
     - Firestore
-    - Storage
+    - Functions
+    - IAM
+    - PubSub
     - Secret Manager
-- Github Actions
+    - Storage
 
-## SMS Notification Rules
+## SMS Notification
+### Rules
 - Minimum 1 hour gap between each message per climate type
 - Trigger only between 6AM - 9PM
-
-## SMS Trigger Criteria
+### Trigger Criteria
 - Ambient room temperature above 75 degrees Fahrenheit or below 65 degrees Fahrenheit
 - Relative humidity above 60% or below 30%
+
+## Interactive Graph
+The results of temperature and humidty readings are calculated for daily averages, and then used at data points in this interactive graph: [![Interactive Graph !](https://img.shields.io/badge/Interactive%20Graph-1abc9c.svg)](http://35.238.110.48:8080)
+
+The graph is hosted on a Google Cloud Compute Engine instance, which has a scheduled cron job that retrieves the latest data aggregated on daily basis from Google Cloud Storage and plots it in the graph. The described process is only the end result of a bigger process, which is wholly explained in this `LaTex` report: [![Report !](https://img.shields.io/badge/Report-1abc9c.svg)](latex/temperature-humidity-report.pdf)
---
+++ target/1.5.0/argon/obj/src/module_info.o.d
@@ -0,0 +1,48 @@
+/home/luexiong/projects/cloud-temperature-humidity-system/target/1.5.0/argon//obj/./src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/assert.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_ansi.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/newlib.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_newlib_version.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/config.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/ieeefp.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/features.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/stdint.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/_default_types.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_intsup.h \
+ /home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_stdint.h
+
+../../../dynalib/inc/module_info.inc:
+
+../../../dynalib/inc/module_info.h:
+
+../../../services/inc/static_assert.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/assert.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_ansi.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/newlib.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/_newlib_version.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/config.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/features.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/stdint.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/machine/_default_types.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_intsup.h:
+
+/home/luexiong/.particle/toolchains/gcc-arm/5.3.1/arm-none-eabi/include/sys/_stdint.h:
